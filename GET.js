f0dafd7641f2851a4f7c5cc60b66478128e2e0bc
TaYaKi71751 <shantian.tw@gmail.com>
2021-10-18 23:55:47 +0900

    Update *.js

--- GET.js
+++ GET.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const cheerio = require("cheerio");
+const _$ = cheerio.load;
 
 const dateFormat = new Intl.DateTimeFormat('ja-JP').format;
 
@@ -9,49 +10,48 @@ function _dataTime(dataTime, a, b) {
     return __d;
 }
 
-function yester(dataTime){
+function yester(dataTime) {
     return dateFormat(new Date(Date.parse(dataTime) - (_24HoursInMillis = 86400000)));
 }
 
+
 function __tpcd(item) {
-    return Object.values(cheerio.load(item)('tpcd').text().split(/[^a-zA-Z]/)).filter((__i) => __i != '')[0];
+    return Object.values(_$(item)('tpcd').text().split(/[^a-zA-Z]/)).filter((__i) => __i != '')[0];
+}
+
+function __sidoNm(item) {
+    return Object.values(_$(item)('sidoNm').text().split(/[^가-힣ㄱ-ㅎㅏ-ㅣ]/).filter((__i) => __i != ''));
 }
 
 function _slice(v) {
     return Object.values(v).slice(0, v.length);
 }
 
-function _Date(_date){
+function _Date(_date) {
     return new Date(Date.parse(_date))
 }
 
 module.exports = {
     "getVaccinationGiven": async function () {
-        return (async function getData() {
-            var data = await axios.get('https://nip.kdca.go.kr/irgd/cov19stats.do?list=all');
-            data = cheerio.load(data.data);
-            const dataTime = _dataTime(dateFormat(_Date(_dataTime(data('dataTime').text(), "\.", "\-"))), "\/", "\-");
+        return (async function getData(type) {
+            const isExistTpcd = typeof type.tpcd != 'undefined';
+            var data = await axios.get(`https://nip.kdca.go.kr/irgd/cov19stats.do?list=${type.list}`);
+            data = _$(data.data);
+            const dataTime = _dataTime(dateFormat(_Date(_dataTime(data('dataTime').text(), "\.", "\-"))), "\/", "\-")
+                , _tpcd = isExistTpcd ? type.tpcd : undefined
+                , _tags = type._tags;
             var items = _slice(data('item'));
             data = {};
+            data['dataTime'] = dataTime;
             items.forEach((item) => {
-                var tpcd = __tpcd(item);
-                const _tpcd = {
-                    'A': { 'today': 'on' },    //Today         TZ=>KST
-                    'B': { 'yesterday': 'cumulative' },   //Yesterday's Cumulative
-                    'C': { 'today': 'cumulative' },    //Today's Cumulative
-                }, _tags = {
-                    "dataTime": "",
-                    "firstCnt": "",
-                    "secondCnt": "",
-                    "thirdCnt": "",
-                };
-                Object.entries(_tpcd[tpcd]).forEach(([k, v]) => {
-                    data[k] = typeof data[k] == 'undefined' ? {} : data[k];
-                    data[k][v] = (function (item) {
+                var tpcd = isExistTpcd ? __tpcd(item) : __sidoNm(item);
+                Object.entries(isExistTpcd ? _tpcd[tpcd] : __sidoNm(item)).forEach(([k, v]) => {
+                    data[isExistTpcd ? k : v] = typeof data[isExistTpcd ? k : v] == 'undefined' ? {} : data[isExistTpcd ? k : v];
+                    data[isExistTpcd ? k : v] = (function (item) {
                         return (function (_xml) {
                             var _Cnts = {};
                             Object.keys(_tags).forEach((_tag) => {
-                                const cnt = cheerio.load(_xml)(_tag);
+                                const cnt = _$(_xml)(_tag);
                                 if (cnt.length == 0) {
                                     _Cnts[_tag] = k.includes('to') ?
                                         dataTime :
@@ -64,6 +64,7 @@ module.exports = {
                         })(item);
                     })(item);
                 });
+
             });
             return data;
         });
--- action.js
+++ action.js
@@ -1,16 +1,50 @@
 const fs = require('fs');
 const path = require('path');
-const dataDir = '/data';
-var jsonPath = undefined;
-function writeJson(data, fileName) {
+const _types = {
+  "simple": {
+    "path": "simple",
+    "list": "all",
+    "tpcd": {
+      'A': { 'today': ""},               //Today         TZ=>KST
+      'B': { 'yesterday_c':'' },   //Yesterday's Cumulative
+      'C': { 'today_c': '' },       //Today's Cumulative
+    }, "_tags": {
+      "dataTime": "",
+      "firstCnt": "",
+      "secondCnt": "",
+      "thirdCnt": "",
+    },
+  }, "regions": {
+    "path": "regions",
+    "list": "sido",
+    "_tags": {
+      "firstCnt": "",
+      "firstTot": "",
+      "secondCnt": "",
+      "secondTot": "",
+      "thirdCnt": "",
+      "thirdTot": "",
+    },
+  },
+};
+async function writeJson(data, dataDir, fileName) {
   fs.writeFileSync(path.resolve(jsonPath = path.join(__dirname, dataDir, (fileName ?? 'latest')) + '.json'), JSON.stringify(data, null, 2));
 }
-(async function () {
-  var _funciton = await require('./GET')['getVaccinationGiven']();
-  return _funciton;
-})()
-  .then((_function) => (_function()))
-  .then((data) => {
-    writeJson(data);
-    writeJson(data,data['today']['on']['dataTime']);
-  });
+async function scrap(type) {
+  const dataDir = `/data/${type.path}`;
+  var jsonPath = undefined;
+
+  (async function () {
+    var _function = await require('./GET')['getVaccinationGiven']();
+    return _function;
+  })()
+    .then((_function) => (_function(type)))
+    .then((data) => {
+      writeJson(data, dataDir);
+      writeJson(data, dataDir, data['dataTime']);
+    });
+}
+
+Object.values(_types).forEach((type) => {
+  scrap(type);
+})
