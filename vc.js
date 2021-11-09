03ba5422fa7aadd7aae4f0d10190f30afe8d0efb
TaYaKi71751 <shantian.tw@gmail.com>
2021-11-09 19:45:46 +0900

    Add V & C

---
+++ vc.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+let cases = fs.readFileSync('./case/latest.json');
+let vaccinations = fs.readFileSync('./vaccination/latest.json');
+cases = JSON.parse(cases);
+vaccinations = JSON.parse(vaccinations);
+let vc = {};
+vc['case'] = cases;
+vc['vaccination'] = vaccinations;
+require('./write')(vc,"./");
