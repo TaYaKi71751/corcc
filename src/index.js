5b20f3406233e2560d0e699b31ab427d8f3588a6
TaYaKi71751 <shantian.tw@gmail.com>
2021-11-10 22:16:03 +0900

    Add title to Each *

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import Page from './app/covid/Page';
 
 // import registerServiceWorker from './registerServiceWorker';
 if(document.location.href.includes("case")){
+  document.querySelector("title").innerHTML = "Cases";
   ReactDOM.render(<Page case="case" />, document.body);
 }else 
 if(document.location.href.includes("vaccination")){
+  document.querySelector("title").innerHTML = "Vaccinations";
   ReactDOM.render(<Page vaccination="vaccination" />, document.body);
 }else {
+  document.querySelector("title").innerHTML = "Vaccinations & Cases";
   ReactDOM.render(<Page case="case" vaccination="vaccination" />, document.body);
 }
 
