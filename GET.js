312b423b16c62e985e413233600e1d225d680070
TaYaKi71751 <shantian.tw@gmail.com>
2021-11-17 20:24:27 +0900

    * *

--- GET.js
+++ GET.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
+const { exec, execSync } = require('child_process');
 const cheerio = require("cheerio");
 const _$ = cheerio.load;
-
+const userAgent = 'Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6';
 const dateFormat = function (date) {
   var d = new Date(date),
     month = '' + (d.getMonth() + 1),
@@ -16,12 +17,16 @@ const dateFormat = function (date) {
   return [year, month, day].join('-');
 };
 
+function curl(url){
+  return execSync(`curl -LsSf ${url} -A "${userAgent}" -o -`).toString();
+}
+
 function yester(dataTime) {
   return dateFormat(new Date(Date.parse(dataTime) - (_24HoursInMillis = 86400000)));
 }
 
 function filterDigit(v) {
-  return v.replaceAll(/[^\d]/g,"");
+  return v.replaceAll(/[^\d]/g, "");
 }
 
 function __tpcd(item) {
@@ -36,59 +41,74 @@ function _slice(v) {
   return Object.values(v).slice(0, v.length);
 }
 
-function mohwTime(_data) {
-  var v = _data('.timetable');
-  v = v.text();
-  v = v.split(/[^\d]/).filter((_) => _ != '');
-  v = (function () {
-    var _tmp = v;
-    _tmp = _tmp[0].length < 3 ? _tmp.slice(1) : _tmp;
-    return `${_tmp[0]}-${_tmp[1]}-${_tmp[2]}`;
-  })();
-  return v;
+function isType(variable, variable_type) {
+  return !(typeof variable != variable_type);
 }
 
-function mohwParseCtNm(_map) {
-  const cityNm = _$(_map)('.cityname').text();
-  return cityNm;
-}
 
-function mohwParseCase(_map) {
-  const _case = {
-    'C': 'confirmed',
-    'R': 'recovered',
-    'D': 'deaths',
-  };
-  var caseData = {};
-  _slice(_$(_map)('li')).forEach((case_) => {
-    const _tit = _$(case_)('span.tit').text()[0];
-    const value = filterDigit(_$(case_)('span.num').text());
-    if (typeof _case[_tit] == 'undefined') { return; }
-    caseData[_case[_tit]] = (value.replaceAll(/[^0-9]/g) == value ? Number(value) : value);
+const innerFind = (function (DOM, ...selectors) {
+  const html = (function (DOM) {
+    return _$(DOM.html());
   });
-  return caseData;
-}
+  selectors = selectors ? (isType(selectors[0], 'string')
+    ? selectors
+    : selectors[0]) : undefined;
+  const _ = html(DOM);
+  return selectors.length ? innerFind(_(selectors[0]), selectors.slice(1, selectors.length)) : _;
+});
 
 function mohwParseCaseDataWCtNm(_map) {
+  const mohwParseCtNm = (function (_map) {
+    const cityNm = _$(_map)('.cityname').text();
+    return cityNm;
+  });
+  const mohwParseCase = (function (_map) {
+    const _case = {
+      '确': 'confirmed',
+      '解': 'recovered',
+      '死': 'deaths',
+      'C': 'confirmed',
+      'R': 'recovered',
+      'D': 'deaths',
+    };
+    var caseData = {};
+    _slice(_$(_map)('li')).forEach((case_) => {
+      const _tit = _$(case_)('span.tit').text().trim();
+      const value = filterDigit(_$(case_)('span.num').text());
+      if(_tit!= _tit.replaceAll(/[A-Za-z]/g,"")){
+        if (typeof _case[_tit[0]] == 'undefined') { return; }
+        caseData[_case[_tit[0]]] = (value.replaceAll(/[^0-9]/g) == value ? Number(value) : value);
+      }else {
+        if (typeof _case[Object.values(_tit).filter((_)=>(_tit.includes(_)))[0]] == 'undefined') { return; }
+        caseData[_case[Object.values(_tit).filter((_)=>(_tit.includes(_)))[0]]] = (value.replaceAll(/[^0-9]/g) == value ? Number(value) : value);
+      }
+    });
+    return caseData;
+  });
+
   var caseWCtNmData = {};
   caseWCtNmData[mohwParseCtNm(_map)] = mohwParseCase(_map);
   return caseWCtNmData;
 }
 
-async function mohwJson(res) {
+function mohwJson(res) {
+  const mohwTime = (function (_data) {
+    var v = innerFind(_data, '.timetable', '.info', 'span').text().trim();
+    v = v.split(/[^\d]/).filter((_) => _ != '');
+    return dateFormat(Date.parse(`${(new Date()).getFullYear()}-${v[v.length - 2]}-${v[v.length - 1]}`));
+  });
   const type = {
     'simple': '.rpsa_detail > div > #mapAll',
     'city': '.rpsa_detail > div > div',
   };
   return Object.fromEntries(Object.entries(type).map(([dataType, selectors]) => {
-    console.log(dataType, selectors)
-    return [dataType, JSON.parse(JSON.stringify(_slice(_$(res.data)(selectors)).map((_map) => {
+    return [dataType, JSON.parse(JSON.stringify(_slice(_$(res)(selectors)).map((_map) => {
       const rtn = mohwParseCaseDataWCtNm(_map);
       if (!dataType.includes("sim")) {
         return rtn;
       }
       return Object.entries(rtn)[0][1];
-    })).replaceAll("},{", ",").replaceAll(/[\[\]]/g, "").replace("{", "{" + `"dataTime":"${mohwTime(_$(res.data))}",`))];
+    })).replaceAll("},{", ",").replaceAll(/[\[\]]/g, "").replace("{", "{" + `"dataTime":"${mohwTime(_$(res))}",`))];
   }));
 }
 
@@ -96,9 +116,11 @@ module.exports = {
   "getVaccinationGiven": async function () {
     return (async function (type) {
       const isExistTpcd = typeof type.tpcd != 'undefined';
-      var data = await axios.get(`https://nip.kdca.go.kr/irgd/cov19stats.do?list=${type.list}`);
+      var data = await axios.get(`https://nip.kdca.go.kr/irgd/cov19stats.do?list=${type.list}`, {
+        headers: { 'User-Agent': 'Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6' }
+      });
       data = _$(data.data);
-      const dataTime = dateFormat(data('dataTime').text().replaceAll(/[(\.|\/)]/g,"-"))
+      const dataTime = dateFormat(data('dataTime').text().replaceAll(/[(\.|\/)]/g, "-"))
         , _tpcd = isExistTpcd ? type.tpcd : undefined
         , _tags = type._tags;
       return JSON.parse(JSON.stringify(_slice(data('item')).map((item) => {
@@ -116,11 +138,12 @@ module.exports = {
         }));
       })).replaceAll("},{", ",").replaceAll(/[\[\]]/g, "").replace("{", `{"dataTime":"${dataTime}",`));
     });
-  }, "getCases": async function () {
-    return (async function (type) {
-      const url = `http://ncov.mohw.go.kr/${type.lang}/bdBoardList.do?brdGubun=${type['brdGubun']}`;
-      const res = await axios.get(url);
-      var data = await mohwJson(res);
+  }, "getCases": function () {
+    return (function (type) {
+      const url = `http://ncov.mohw.go.kr/${type.lang ? (type.lang + '/') : ''}bdBoardList.do?brdGubun=${type['brdGubun']}`;
+      console.log(url);
+      const res = execSync(`curl -LsSf ${url} -A "${userAgent}" -o -`).toString();
+      var data = mohwJson(res);
       return data;
     });
   },
--- case.js
+++ case.js
@@ -1,37 +1,34 @@
 const { exit } = require('process');
 const writeJson = require('./write');
 const _types = {
-  "getCases": {
-    'lang': 'en',
-    'brdGubun': '162'
-  },
+  "getCases":
+    [
+      {
+        'lang': 'en',
+        'brdGubun': '162'
+      },
+      // {
+      //   'lang': 'cn',
+      //   'brdGubun': '262'
+      // },
+    ],
 };
 
 function scrap(fnNm, type) {
   const dataDir = `/case/`;
-  var jsonPath = undefined;
-
-  (async function () {
-    var _function = await require('./GET')[fnNm]();
-    return _function;
-  })()
-    .then((_function) => (_function(type)))
-    .catch((error) => {
-      console.error(error);
-      exit(-1);
-    })
-    .then((data)=>(data))
-    .then((data) => {
-      Object.entries(data).forEach(([k,v])=>{
-        if(k=='simple'){
-          writeJson(v, dataDir);
-        }
-        writeJson(v, dataDir+k);
-        writeJson(v, dataDir+k, v['dataTime']);
-      })
-    });
+  var _function = require('./GET')[fnNm]();
+  const data = _function(type);
+  Object.entries(data).forEach(([k, v]) => {
+    if (k == 'simple') {
+      writeJson(v, dataDir);
+    }
+    writeJson(v, dataDir + k);
+    writeJson(v, dataDir + k, v['dataTime']);
+  })
 }
 
 Object.entries(_types).forEach(([fnNm, v]) => {
-  scrap(fnNm, v);
+  v.forEach(v => {
+    scrap(fnNm, v);
+  })
 })
