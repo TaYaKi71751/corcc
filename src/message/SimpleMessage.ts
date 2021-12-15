11f3c87796576120bf16e9ef5ed07e241813364b
TaYaKi71751 <shantian.tw@gmail.com>
2021-12-16 01:13:14 +0900

    Add .slack.md

--- src/message/SimpleMessage.ts
+++ src/message/SimpleMessage.ts
@@ -32,30 +32,61 @@ const toMarkdown = function (paths: string[]) {
       })(table_keys.length)}` + '\n' +
       `${table_values.join('|')}`
     );
-    const msgPath = path.replace('latest', 'markdown').replace('json', 'md');
+    console.log(md);
+    const mdPath = path.replace('latest', 'markdown').replace('.json', '.md');
     try {
-      execSync(`ls ${pwd}/${msgPath.substring(0, msgPath.lastIndexOf('/'))}`).toString()
+      execSync(`ls ${pwd}/${mdPath.substring(0, mdPath.lastIndexOf('/'))}`).toString()
     } catch (e) {
       console.error(new String(e));
       try {
-        execSync(`mkdir -p ${pwd}/${msgPath.substring(0, msgPath.lastIndexOf('/'))}`).toString();
+        execSync(`mkdir -p ${pwd}/${mdPath.substring(0, mdPath.lastIndexOf('/'))}`).toString();
       } catch (e) {
         exit(0xDD);
       }
     } finally {
       try {
-        execSync(`ls ${pwd}/${msgPath.substring(0, msgPath.lastIndexOf('/'))}/.gitkeep`).toString();
+        execSync(`ls ${pwd}/${mdPath.substring(0, mdPath.lastIndexOf('/'))}/.gitkeep`).toString();
       } catch (e) {
         console.error(new String(e));
         try {
-          execSync(`echo ''> ${pwd}/${msgPath.substring(0, msgPath.lastIndexOf('/'))}/.gitkeep`);
+          execSync(`echo ''> ${pwd}/${mdPath.substring(0, mdPath.lastIndexOf('/'))}/.gitkeep`);
         } catch (e) {
           exit(0xDF);
         }
       }
     }
-    fs.writeFileSync(`${pwd}/${msgPath}`, md);
-    return md;
+    fs.writeFileSync(`${pwd}/${mdPath}`, md);
+    const slackMarkdown = Object.entries(json).map(([k, v]) => {
+      const key = k;
+      const value = v;
+      return `*${k}*\n\r- ${v}`;
+    });
+    console.log(slackMarkdown.join('\n'));
+    const slackMarkdownPath = path.replace('latest', 'markdown').replace('.json', '.slack.md');
+
+    try {
+      execSync(`ls ${pwd}/${slackMarkdownPath.substring(0, slackMarkdownPath.lastIndexOf('/'))}`).toString()
+    } catch (e) {
+      console.error(new String(e));
+      try {
+        execSync(`mkdir -p ${pwd}/${slackMarkdownPath.substring(0, slackMarkdownPath.lastIndexOf('/'))}`).toString();
+      } catch (e) {
+        exit(0xDD);
+      }
+    } finally {
+      try {
+        execSync(`ls ${pwd}/${slackMarkdownPath.substring(0, slackMarkdownPath.lastIndexOf('/'))}/.gitkeep`).toString();
+      } catch (e) {
+        console.error(new String(e));
+        try {
+          execSync(`echo ''> ${pwd}/${slackMarkdownPath.substring(0, slackMarkdownPath.lastIndexOf('/'))}/.gitkeep`);
+        } catch (e) {
+          exit(0xDF);
+        }
+      }
+    }
+    fs.writeFileSync(`${pwd}/${slackMarkdownPath}`, slackMarkdown.join('\n'));
+    return [md, slackMarkdown];
   })
 }
 export { toMarkdown }
