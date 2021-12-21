0d0d521bb9648f217aec4adee24f88cb34f121fc
TaYaKi71751 <shantian.tw@gmail.com>
2021-12-21 20:11:15 +0900

    Use find to getPath

--- src/badge/SVGBadge.ts
+++ src/badge/SVGBadge.ts
@@ -4,7 +4,6 @@ import path from 'path';
 import JSONBig from 'json-bigint';
 import { Utilities } from '../util/Utilities';
 import { emoji } from './Emoji';
-import __path__ from 'path';
 import {
     pwd,
     cdPath,
@@ -13,13 +12,38 @@ import {
 const thousands = require('thousands');
 import { tryCatch } from '../util/TryCatch'
 
-const util = new Utilities();
-
+const u = (() => {
+    return new Utilities();
+})();
+type s = string;
+type a<T> = Array<T>;
+type n = number;
+type b = boolean;
+type f = Function;
 function readCssFile({
     cssPath
 }: any): string {
-    const __path__ = path.resolve(path.join(__dirname, cssPath));
-    return fs.readFileSync(`${__path__}`).toString();
+    const r = ((_p, d, p) => {
+        return ((f: f, a: s) => {
+            return f(a);
+        })(_p.resolve, ((f: f, a: s, b: s) => {
+            return f(a, b)
+        })(_p.join, d, p));
+    })(path, __dirname, cssPath);
+    const a: s[] = ((p: s): s[] => {
+        const c: s = `find ${p} -name *.css -exec echo {} \\\;`;
+        const r: s = ((c): s => {
+            return execSync(c).toString();
+        })(c);
+        const a: s[] = ((a: s[]) => {
+            return a.filter((_) => (_ != ''));
+        })(r.split('\n'));
+        return a;
+    })(r);
+    const c: s[] = a.map((p: s): s => {
+        return fs.readFileSync(p, 'utf8').toString();
+    });
+    return c.join('\n');
 }
 
 function getForeignObject({
@@ -27,11 +51,11 @@ function getForeignObject({
     value
 }: any) {
     const foreignObject = `<div xmlns="http://www.w3.org/1999/xhtml">
-        <div class="container">
-            <div class="${title}">
+        <div class="${title} container">
+            <div class="${title} title">
                 ${title}
             </div>
-            <div class="${title}">
+            <div class="${title} value">
                 ${value}
             </div>
         </div>
@@ -42,44 +66,90 @@ function getSvg({
     title,
     value
 }: any) {
-    const svg = `<svg viewBox="0 0 120 25" xmlns="http://www.w3.org/2000/svg">
+    const svg = `<svg viewBox="0 0 ${2 +
+        16
+        + 2 +
+        (15 * value.length)
+        + 2
+        } 25" xmlns="http://www.w3.org/2000/svg">
 	<style>
     ${readCssFile({
-        cssPath: './res/css/style.css'
-    })}
+            cssPath: './res/css/'
+        })}
 	</style>
-	<svg viewBox="0 0" height="27" />
-	<foreignObject width="100%" height="27">
+	<foreignObject width="100%" height="100%">
     ${getForeignObject({
-        title,
-        value
-    })}
+            title,
+            value
+        })}
 	</foreignObject>
     </svg>`;
     return svg;
 }
 
-const saveBadges = (path: any) => {
-    const read = fs.readFileSync(path).toString();
+const g = {
+    s: getSvg
+}
+
+const saveBadges = (jsonPath: string) => {
+    const read = ((f: f, p: s) => {
+        return f(p).toString();
+    })(fs.readFileSync, jsonPath);
     const json = JSONBig.parse(read);
     Object.entries(json).forEach(([k, v]) => {
-        const badgeSavePath = __path__.join(pwd, (path.replace('latest', 'badge').replace('.json', `/${k}.svg`)));
-        const titleEmoji = emoji[k];
-        const svgBadge = getSvg({
-            title: `${titleEmoji}`,
-            value: util.isNumberOnly(`${v}`) ? thousands(v) : v
-        });
+        const P = (b: s, c: s) => {
+            return ((a: s, c: n): s => {
+                return a.substring(0, c);
+            })(b, ((a: s, c: s): n => {
+                return a.lastIndexOf(c);
+            })(b, c));
+        };
+        const b = (() => {
+            const f = ((j: s): s => {
+                return path.join(pwd, ((p: s, k: s) => {
+                    const r = (o: s, a: s, b: s): s => {
+                        return o.replace(a, b);
+                    }
+                    return ((p: s, k: s): s => {
+                        return r(p, '.json', `/${k}.svg`);
+                    })(((p: s): s => {
+                        return r(p, 'latest', 'badge')
+                    })(p), k);
+                })(j, k));
+            })(jsonPath);
+            const e = ((e, n: s) => {
+                return e[n];
+            })(emoji, k);
+            const s = ((f, p) => {
+                return f(p);
+            })(g.s, {
+                title: `${e}`,
+                value: ((a: b, b: s, c: f) => {
+                    return a ? c(b) : b;
+                })(((a) => {
+                    return u.isNumberOnly(a);
+                })(`${v}`), `${v}`, thousands)
+            })
+            const d = ((p: s) => {
+                return P(p, '/')
+            })(f);
+            return {
+                f, d, s
+            };
+        })();
         tryCatch({
             func: mkdirPath,
-            params: badgeSavePath.substring(0, badgeSavePath.lastIndexOf('/')),
+            params: b.d,
             catchFunc: (() => { }),
         })
         tryCatch({
             func: execSync,
-            params: `echo '' > ${badgeSavePath.substring(0, badgeSavePath.lastIndexOf('/'))}/.gitkeep`,
+            params: ((p: s) => {
+                `echo '' > ${p}/.gitkeep`
+            })(b.d),
             catchFunc: (() => { }),
         })
-        fs.writeFileSync(badgeSavePath, svgBadge);
+        fs.writeFileSync(b.f, b.s);
     })
 }
 
