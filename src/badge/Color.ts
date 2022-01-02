d9f620794b9d96d7099121759bd80985aac59fc3
TaYaKi71751 <shantian.tw@gmail.com>
2022-01-02 22:52:14 +0900

    Use badgen

---
+++ src/badge/Color.ts
@@ -0,0 +1,19 @@
+const colors: any = {
+	'confirmed': '#6a3',
+	'🦠': '#6a3',
+	'deaths': '#aaa',
+	'💀': '#aaa',
+	'recovered': '#f8a',
+	'😊': '#f8a',
+	'dataTime': '#d55',
+	'📅': '#d55',
+	'vaccination': '#ddd',
+	'💉': '#ddd',
+	'firstCnt': '#f78',
+	'☝️': '#f78',
+	'secondCnt': '#f9a',
+	'✌️': '#f9a',
+	'thirdCnt': '#fa4',
+	'🤟': '#fa4',
+};
+export {colors};
