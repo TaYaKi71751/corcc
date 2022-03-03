c6405fc3d6a4bac4ea7aa733f3588684a1d2f673
TaYaKi71751 <shantian.tw@gmail.com>
2022-03-04 07:11:23 +0900

    Fix Tweet.ts

--- src/util/Tweet.ts
+++ src/util/Tweet.ts
@@ -1,37 +1,44 @@
 import Twitter from 'twitter';
 const client = new Twitter({
-	consumer_key: process.env.TWITTER_CONSUMER_KEY,
-	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+	consumer_key: `${process.env.TWITTER_CONSUMER_KEY}`,
+	consumer_secret: `${process.env.TWITTER_CONSUMER_SECRET}`,
+	access_token_key: `${process.env.TWITTER_ACCESS_TOKEN_KEY}`,
+	access_token_secret: `${process.env.TWITTER_ACCESS_TOKEN_SECRET}`
 });
-const oe = Object.entries;
-const fe = Object.fromEntries;
 const MAX_TWEET_TEXT_BYTES = 140;
 export async function updateTweet ({
 	status,
 	replyId
 }: {
 	status: string;
-	replyId?: undefined | string;
+	replyId?: string;
 }) {
-	const m = !(status.length < MAX_TWEET_TEXT_BYTES);
-	const n = status.lastIndexOf('\n', MAX_TWEET_TEXT_BYTES);
-	if (m && (n < 0)) {
-		throw Error('Need \\n to split Tweet');
-	}
-	const s = m ? status.substring(0, n) : status;
-
-	const result = await client.post('statuses/update', fe(oe({
-		status: s,
-		in_reply_to_status_id_str: replyId
-	}).filter((_:any):any => (_))));
-	if (m) {
-		const o = status.substring(n, status.length);
-		const tweetIdStr = result.id_str;
-		await updateTweet({
-			status: o,
-			replyId: tweetIdStr
-		});
+	try {
+		const m = !(status.length < MAX_TWEET_TEXT_BYTES);
+		const n = status.lastIndexOf('\n', MAX_TWEET_TEXT_BYTES);
+		if (m && (n < 0)) {
+			throw Error('Need \\n to split Tweet');
+		}
+		const s = m ? status.substring(0, n) : status;
+		const o:any = {
+			status: s
+		};
+		if (replyId) {
+			o.in_reply_to_status_id_str = replyId;
+		}
+		console.log(o);
+		const result:any = await client.post('statuses/update', o);
+		if (m) {
+			const o = status.substring(n, status.length);
+			const tweetIdStr = result.id_str;
+			console.log(o);
+			await updateTweet({
+				status: o,
+				replyId: tweetIdStr
+			});
+		}
+	} catch (e:any) {
+		console.error(e);
+		throw e;
 	}
 }
